b7b911d5e343042b44f370f68b6624cf1867abf2
Margie Fuertes <85895396+MFuertes1012@users.noreply.github.com>
2021-10-28 10:22:18 -0600

    Add files via upload

---
+++ Infrastructure/ActivosFijos/ActivoFijoModel.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.ActivosFijos
+{
+    public class ActivoFijoModel : IModel<ActivoFijoModel>
+    {
+        protected ActivoFijoModel[] data;
+
+        public void Add(ActivoFijoModel t, ref ActivoFijoModel[] data)
+        {
+            if (data == null)
+            {
+                data = new ActivoFijoModel[1];
+                data[0] = t;
+                return;
+            }
+
+            ActivoFijoModel[] tmp = new ActivoFijoModel[data.Length + 1];
+            Array.Copy(data, tmp, data.Length);
+            tmp[tmp.Length - 1] = t;
+            data = tmp;
+        }      
+
+        public void Create(ActivoFijoModel t)
+        {
+            Add(t, ref data);
+        }
+
+        ActivoFijoModel[] IModel<ActivoFijoModel>.FindAll()
+        {
+            return data;
+        }
+    }
+}
---
+++ Infrastructure/ActivosFijos/Depreciacion.cs
@@ -0,0 +1,54 @@
+﻿using Domain.Entities.Activos;
+using Domain.Enums.Activo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.ActivosFijos
+{
+    public class Depreciacion
+    {
+        public decimal[] Depreciar(ActivoFijo af)
+        {
+            switch (af.MetodoDepreciacion)
+            {
+                case MetodoDepreciacion.LineaRecta:
+                    return DepreciacionLineaRecta(af);
+                case MetodoDepreciacion.SDA:
+                    return DepreciacionSDA(af);
+            }
+            return null;
+        }
+        private decimal[] DepreciacionLineaRecta(ActivoFijo activoFijo)
+        {
+            int vidaUtil = VidaUtil(activoFijo.TipoActivoFijo);
+            return Enumerable.Repeat(activoFijo.ValorActivo / vidaUtil, vidaUtil).ToArray();
+        }
+
+        private decimal[] DepreciacionSDA(ActivoFijo activoFijo)
+        {
+            return null;
+        }
+
+        private int VidaUtil(TipoActivoFijo tipoActivoFijo)
+        {
+            switch (tipoActivoFijo)
+            {
+                case TipoActivoFijo.Edificio:
+                    return 25;
+                case TipoActivoFijo.Maquinaria:
+                    return 5;
+                case TipoActivoFijo.EquipoDeComputo:
+                    return 1;
+                case TipoActivoFijo.Mobiliario:
+                    return 2;
+                case TipoActivoFijo.Vehiculo:
+                    return 6;
+                default:
+                    throw new ArgumentException("Tipo de activo fijo no existe.");
+            }
+        }
+    }
+}
