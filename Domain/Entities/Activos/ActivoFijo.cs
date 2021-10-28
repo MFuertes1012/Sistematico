4848dc4eb8d923a0315f596caeb64cf66b7ca679
Margie Fuertes <85895396+MFuertes1012@users.noreply.github.com>
2021-10-28 10:21:59 -0600

    Add files via upload

---
+++ Domain/Entities/Activos/ActivoFijo.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Enums.Activo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Activos
+{
+    public class ActivoFijo
+    {
+        public int Id { get; set; }
+        public string CodigoActivo { get; set; }
+        public string NombreActivo { get; set; }
+        public string Descripcion { get; set; }
+        public decimal ValorActivo { get; set; }
+        public DateTime FechaAdquisicion { get; set; }
+        public TipoActivoFijo TipoActivoFijo { get; set; }
+        public MetodoDepreciacion MetodoDepreciacion { get; set; }
+    }
+}
